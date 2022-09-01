bc5b08504cbadd9fdedcba065080dc7fe37d45e0
Sumithreddy55 <108969543+Sumithreddy55@users.noreply.github.com>
2022-09-01 18:03:55 +0530

    Add files via upload

---
+++ split.java
@@ -0,0 +1,9 @@
+public class split{  
+public static void main(String args[]){  
+String s1="HI! I'm shivkumar Gupta and my hobbies are cricket,taveling,listening music  and many more thing,Thank you.";  
+String[] words=s1.split("\\s");//splits the string based on whitespace  
+//using java foreach loop to print elements of string array  
+for(String w:words){  
+System.out.println(w);  
+}  
+}}
